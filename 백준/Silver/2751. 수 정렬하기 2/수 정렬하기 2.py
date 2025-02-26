29f37e0d3846256a28612036c0bd0d38858f0559
Beommo Kim <67816087+bbeomdev@users.noreply.github.com>
2025-02-26 11:06:59 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1320 ms, Memory: 86084 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -1,12 +1,4 @@
 import sys
-n = int(sys.stdin.readline().strip())
-num_dict = {}
-for i in range(n):
-    num = int(sys.stdin.readline().strip())
-    if num in num_dict:
-        num_dict[num] += 1
-    else:
-        num_dict[num] = 1
-for i in sorted(num_dict.keys()):
-    for i2 in range(num_dict[i]):
-        print(i)
+n = int(sys.stdin.readline())
+sorted_num = sorted([int(sys.stdin.readline()) for _ in range(n)])
+for i in sorted_num: print(i)
