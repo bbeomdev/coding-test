054decac82cb10863be07d835f97ede74403e5e3
Beommo Kim <67816087+bbeomdev@users.noreply.github.com>
2025-03-06 15:13:35 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -2,47 +2,29 @@ from collections import deque
 
 
 n,m = map(int, input().split())
+
 graph = [[0] + list(map(int, input())) for _ in range(n)]
 graph.insert(0, [0]+[0]*m)
+
 bfs_visit = [[False] + [False] * m for _ in range(n)]
 bfs_visit.insert(0, [False] + [False] * m)
+
 first = (1,1)
 q = deque()
 q.append(first)
 bfs_visit[1][1] = True
+direct = [(-1,0), (1,0), (0,-1), (0,1)]
+
 while q:
     x,y = q.popleft()
     graph_num = graph[x][y]
-    try:
-        if graph[x-1][y] != 0 and bfs_visit[x-1][y] == False:
-            q.append((x-1,y))
-            graph[x-1][y] += graph_num
-            bfs_visit[x-1][y] = True
-    except:
-        pass
-
-    try:
-        if graph[x+1][y] != 0 and bfs_visit[x+1][y] == False:
-            q.append((x+1,y))
-            graph[x+1][y] += graph_num
-            bfs_visit[x+1][y] = True
-    except:
-        pass
-
-    try:
-        if graph[x][y-1] != 0 and bfs_visit[x][y-1] == False:
-            q.append((x,y-1))
-            graph[x][y-1] += graph_num
-            bfs_visit[x][y-1] = True
-    except:
-        pass
-
-    try:
-        if graph[x][y+1] != 0 and bfs_visit[x][y+1] == False:
-            q.append((x,y+1))
-            graph[x][y+1] += graph_num
-            bfs_visit[x][y+1] = True
-    except:
-        pass
+    for dx, dy in direct:
+        try:
+            if graph[x+dx][y+dy] != 0 and bfs_visit[x+dx][y+dy] == False:
+                q.append((x+dx,y+dy))
+                graph[x+dx][y+dy] += graph_num
+                bfs_visit[x+dx][y+dy] = True
+        except:
+            pass
 
 print(graph[n][m])
