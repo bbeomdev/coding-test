f70381891bd775f71b43e6b143400ff131d3adc4
Beommo Kim <67816087+bbeomdev@users.noreply.github.com>
2025-03-09 18:15:16 +0900

    [Silver IV] Title: 수 찾기, Time: 512 ms, Memory: 48608 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,19 +1,19 @@
 def binary(n_list, target, start, end):
+    while start <= end:
+        mid = int((start+end) / 2)
 
-    if start > end:
-        return 0
+        if n_list[mid] == target:
+            return 1
+        
+        elif n_list[mid] > target:
+            end = mid-1
+        
+        else:
+            start = mid+1
+    return 0
+
+
     
-    mid = int((start+end) / 2)
-    if n_list[mid] == target:
-        return 1
-    
-    elif n_list[mid] > target: # target이 mid보다 왼쪽에 있는경우
-        return binary(n_list, target, start, mid-1)
-
-    elif n_list[mid] < target: # target이 mid보다 오른쪽에 있는경우
-        return binary(n_list, target, mid+1, end)
-
-
 n = int(input())
 n_list = list(map(int, input().split()))
 m = int(input())
